490e3bb5a258ccf4232697088aca3966806f7deb
Phil Yeeles <phil@yeel.es>
2024-10-04 14:39:40 +0100

    render-string and render-multiline-string no longer update the position after rendering; added tests to see this in action; updated screenshot

--- text.f
+++ text.f
@@ -15,17 +15,23 @@ cream12 cream12len fileread drop
 \ neater position management
 variable posx 1 cells allot \ current x coordinate
 variable posy 1 cells allot \ current y coordinate
+\ get x coordinate
+: get-x ( -- x)
+    posx @ ;
+\ get y coordinate
+: get-y ( -- y)
+    posy @ ;
 \ set x coordinate
 : set-x ( x -- )
     posx ! ;
 \ set y coordinate
-: set-y ( x -- )
+: set-y ( y -- )
     posy ! ;
 \ increment x coordinate
 : inc-x ( x -- )
     posx @ + posx ! ;
 \ increment y coordinate
-: inc-y ( x -- )
+: inc-y ( y -- )
     posy @ + posy ! ;
 \ update position based on current values of posx and posy
 : update-position ( -- )
@@ -43,7 +49,7 @@ variable posy 1 cells allot \ current y coordinate
 \ render one character tile at address a in sprite mode x, leaving its width w (in pixels) on the stack
 : render-char-tile ( a x -- w )
     swap spritedata sprite ;
-\ render character c in sprite mode x
+\ render character c in sprite mode x, advancing x position by the character's width and leaving the y position untouched
 : render-character ( c x -- )
     swap                      \ x c
     dup h# 20 < if
@@ -75,9 +81,10 @@ variable posy 1 cells allot \ current y coordinate
       update-position
     loop
     nip nip ;                 \ w
-\ render string at address a with length u in sprite mode x
+\ render string at address a with length u in sprite mode x, leaving position untouched
 : render-string ( a u x -- )
-    swap 0 do          \ a x
+    0 get-x get-y 2rot 2rot drop \ back up old x and y values
+    swap 0 do          \ a x (ignoring old x and y for clarity)
       swap dup i + c@  \ x a c
       rot dup          \ a c x x
       2swap            \ x x a c
@@ -85,10 +92,12 @@ variable posy 1 cells allot \ current y coordinate
       render-character \ x a w
       drop swap        \ a x
     loop
-    drop drop ;
-\ render string at address a with length u in sprite mode x with line breaks at set width w (in pixels)
+    drop drop          \ old x and old y left on the stack
+    set-y set-x update-position ; \ return to original position
+\ render string at address a with length u in sprite mode x with line breaks at set width w (in pixels), leaving position untouched
 : render-multiline-string ( a u x w -- )
-    2swap                 \ x w a u
+    get-x get-y 2rot 2rot \ back up old x and y values
+    2swap                 \ x w a u (ignoring old x and y for clarity)
     0                     \ x w a u acc (width accumulator)
     swap 0 do             \ x w a acc
       rot                 \ x a acc w
@@ -114,7 +123,8 @@ variable posy 1 cells allot \ current y coordinate
       swap                \ a acc x w
       2swap               \ x w a acc
     loop
-    drop drop drop drop ;
+    drop drop drop drop   \ old x and old y left on the stack
+    set-y set-x update-position ; \ return to original position
 
 \ testing
 \ newline function for test purposes
@@ -134,9 +144,13 @@ test-newline
 " Where will Mike make his machines?" h# 43 render-string \ string with wide characters (W, M, w, m)
 test-newline
 test-newline
-" Xyzzy" h# 41 500 render-multiline-string \ string that doesn't hit the width limit
+" Xyzzy" h# 01 500 render-multiline-string \ string that doesn't hit the width limit
+1 inc-x 1 inc-y update-position
+" Xyzzy" h# 4a 500 render-multiline-string \ string that doesn't hit the width limit
 test-newline
 test-newline
-" Where will Mike make his machines?" h# 41 100 render-multiline-string \ string that does hit the width limit
+" Where will Mike make his machines?" h# 01 100 render-multiline-string \ string that does hit the width limit
+1 inc-x 1 inc-y update-position
+" Where will Mike make his machines?" h# 4f 100 render-multiline-string \ string that does hit the width limit
 .s
 brk
